38a0bb77a8b141e42165364e34322e6a13545f13
Juan Feris <feris.juan@gmail.com>
2020-12-04 11:48:17 -0500

    First Controller

--- backend/routes/blog.js
+++ backend/routes/blog.js
@@ -1,8 +1,8 @@
 const express = require('express')
 const router = express.Router()
 
-router.get('/', (req, res) => {
-	res.json({ time: Date().toString() })
-})
+const { time } = require('../controllers/blog')
+
+router.get('/', time)
 
 module.exports = router
