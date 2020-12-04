54d2ba469803543aeeb51da0bf7ea9cce58301ff
Juan Feris <feris.juan@gmail.com>
2020-12-04 12:15:25 -0500

    Basic Aurh Route

--- backend/server.js
+++ backend/server.js
@@ -8,6 +8,7 @@ require('dotenv').config()
 
 //routes
 const blogRoutes = require('./routes/blog')
+const authRoutes = require('./routes/auth')
 
 // app
 const app = express()
@@ -29,6 +30,7 @@ app.use(cookieParser())
 
 // routes middleware
 app.use('/api', blogRoutes)
+app.use('/api', authRoutes)
 
 // CORS
 if (process.env.NODE_ENV === 'development') {
