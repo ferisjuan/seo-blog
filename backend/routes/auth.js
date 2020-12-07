f565c971136f8383bc4d2b1e177a33fb0b2309e2
Juan Feris <feris.juan@gmail.com>
2020-12-07 12:04:14 -0500

    Renaming File

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,12 +1,12 @@
 const express = require('express')
 const router = express.Router()
 
-const { signup } = require('../controllers/signup')
+const { auth } = require('../controllers/auth')
 
 // validators
 const { runValidation } = require('../validators')
 const { userSignupValidator } = require('../validators/auth')
 
-router.post('/signup', userSignupValidator, runValidation, signup)
+router.post('/signup', userSignupValidator, runValidation, auth)
 
 module.exports = router
