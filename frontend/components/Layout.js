8fd036b36b8c86d6564d7216929893781c5019d0
Juan Feris <feris.juan@gmail.com>
2020-12-15 17:11:12 -0500

    Added Nextjs and Basic Page and Template

---
+++ frontend/components/Layout.js
@@ -0,0 +1,11 @@
+const Layout = ({ children }) => {
+	return (
+		<>
+			<header>Header</header>
+			{children}
+			<footer>Footer</footer>
+		</>
+	)
+}
+
+export default Layout
---
+++ frontend/pages/index.js
@@ -0,0 +1,9 @@
+import Layout from '../components/Layout'
+
+const Index = () => (
+	<Layout>
+		<h2>Index Page</h2>
+	</Layout>
+)
+
+export default Index
