2ba8262924f467069a49008750e0af6ef0e3d173
Juan Feris <feris.juan@gmail.com>
2020-12-22 10:26:07 -0500

    Removing Elements from Navbar

--- frontend/components/Header.js
+++ frontend/components/Header.js
@@ -30,22 +30,6 @@ const Header = props => {
 						<NavItem>
 							<NavLink href='/components/'>Components</NavLink>
 						</NavItem>
-						<NavItem>
-							<NavLink href='https://github.com/reactstrap/reactstrap'>
-								GitHub
-							</NavLink>
-						</NavItem>
-						<UncontrolledDropdown nav inNavbar>
-							<DropdownToggle nav caret>
-								Options
-							</DropdownToggle>
-							<DropdownMenu right>
-								<DropdownItem>Option 1</DropdownItem>
-								<DropdownItem>Option 2</DropdownItem>
-								<DropdownItem divider />
-								<DropdownItem>Reset</DropdownItem>
-							</DropdownMenu>
-						</UncontrolledDropdown>
 					</Nav>
 					<NavbarText>Simple Text</NavbarText>
 				</Collapse>
